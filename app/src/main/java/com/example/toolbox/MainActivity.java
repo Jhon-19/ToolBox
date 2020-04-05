f54213bad45f05893f1ca6c302fd05c16357ad78
Jhon <1723368058@qq.com>
2020-04-05 10:16:32 +0800

    test

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("http://bkjw.whu.edu.cn/stu/stu_index.jsp"));
+                //wer
                 startActivity(Intent.createChooser(intent, "访问教务系统"));
             }
         });
